c26239c3616250727e5acac756c66831fd1c8924
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-15 12:59:46 +0300

    Add post component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,17 @@
 export default function Header() {
-    return<div>Header</div>
+  return (
+    <header className="header">
+      <h1 className="header__title">Twitt</h1>
+      <nav>
+        <ul className="header__navigation">
+          <li>
+            <p>Posts</p>
+          </li>
+          <li>
+            <p>MyProfile</p>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
