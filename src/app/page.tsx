7b6b9c50c96b3decfc86e274d4dd14542a8c9bfd
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-17 16:53:03 +0300

    Add backend pots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+'use client';
+
+import { useGetAllPostsQuery } from '@Redux/services/backendAPI';
+import { PostType } from '@Redux/services/backendTypes';
 import Header from '@components/Header/Header';
 import MainModal from '@components/MainModal/MainModal';
 import Post from '@components/Post/Post';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const { data } = useGetAllPostsQuery();
+
+  useEffect(() => {
+    if (data) setPosts(data);
+  }, [data]);
+  console.log(data);
   return (
     <>
       <Header />
       <main className="main__container">
-        <ul className="posts__list">
-          <li>
-            <Post />
-          </li>
+        <ul className="posts__list">{
+          posts.map((el) => <li key={el._id}><Post { ...el} /></li>)
+        }
         </ul>
       </main>
     </>
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,19 +1,16 @@
-export default function Post() {
+import { PostType } from '@Redux/services/backendTypes';
+
+export default function Post({ text, likesNumber, img, liked }: PostType) {
   return (
     <div className="post">
       <div className="post__container--start">
         <div className="post__avatar">Av</div>
         <p>User</p>
       </div>
-      <p className="post__text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat at
-        provident, mollitia ad, velit magnam fuga ab officiis aut amet dolorem
-        sapiente perferendis, voluptatem reiciendis fugit earum quasi
-        voluptatibus esse?
-      </p>
-      <div className="post__content">content</div>
+      <p className="post__text">{text}</p>
+      <div className="post__content">{img ? img : ''}</div>
       <div className="post__container--space">
-        <p>2 Likes</p>
+        <p>{`${likesNumber} ${likesNumber === 1 ? 'Like' : 'Likes'}`}</p>
         <p>comments</p>
       </div>
     </div>
